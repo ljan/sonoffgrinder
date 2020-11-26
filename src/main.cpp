44bd73fe807ac18dd75b3d1d3991a5afa1c3312a
Jan <schick.ne.email@gmail.com>
2020-11-26 17:20:09 +0100

    OLED Support
    Added Support for SSD1306 OLED on RX and TX pin
    Removed Serial Output
    WiFi Optimization

--- src/main.cpp
+++ src/main.cpp
@@ -8,28 +8,39 @@
 // OTA
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+// OLED
+//#include "SSD1306Wire.h"
+#include "SSD1306Brzo.h"
 
 #define ON LOW    // LOW is LED ON
 #define OFF HIGH  // HIGH is LED OFF
 
+#define TX 1 // Pin 1 on Sonoff
+#define RX 3 // Pin 3 on Sonoff
+
+#define VERSION "v0.3-beta"
+
 //======SERVER PART======================================================
-const char* ssid     = "UPC66F2A88";
-const char* password = "c2ncfVemwpnQ";
+const char* ssid     = "kaffee";
+const char* password = "kaffeekaffee";
 ESP8266WebServer server ( 80 );
 char htmlResponse[3000];
 //======LOKALER PART======================================================
 os_timer_t myTimer;
-bool tickOccured = false;
+bool clickOccured = false;
 bool pressOccured = false;
 bool wifiStatus = false;
 int grinderPin = 12;
 int ledPin = 13;
 TTBOUNCE button = TTBOUNCE(14);  // GPIO 14, last PIN on Header
+SSD1306Brzo  display(0x3c, RX, TX); // ADDRESS, SDA, SCL
 
 int time_ss = 1000;
 int time_ds = 2000;
 int time_max = 15000;
 int holdTime = 0;
+int relaisTime = 0;
+unsigned long relaisStart = 0;
 
 unsigned int debounceInterval = 50;
 unsigned int pressInterval = 500;
@@ -38,15 +49,18 @@ void click() {
   if (digitalRead(grinderPin) == LOW) {
     digitalWrite(grinderPin, HIGH);  // turn Relais ON
     os_timer_arm(&myTimer, time_ss, false);
-    Serial.println("Clicked");
-    Serial.println("Relais " + String(time_ss, DEC) + " ms ON");
+    clickOccured = true;
+    relaisTime = time_ss;
+    relaisStart = millis();
+    //Serial.println("Clicked");
+    //Serial.println("Relais " + String(time_ss, DEC) + " ms ON");
     digitalWrite(ledPin, ON);
   } else {
-    tickOccured = false;
     digitalWrite(grinderPin, LOW);
     os_timer_disarm(&myTimer);
-    Serial.println("Abort!");
-    Serial.println("Relais OFF");
+    clickOccured = false;
+    //Serial.println("Abort!");
+    //Serial.println("Relais OFF");
     digitalWrite(ledPin, OFF);
   }
 }
@@ -55,15 +69,18 @@ void doubleClick() {
   if (digitalRead(grinderPin) == LOW) {
     digitalWrite(grinderPin, HIGH);  // turn Relais ON
     os_timer_arm(&myTimer, time_ds, false);
-    Serial.println("DoubleClicked");
-    Serial.println("Relais " + String(time_ds, DEC) + " ms ON");
+    clickOccured = true;
+    relaisTime = time_ds;
+    relaisStart = millis();
+    //Serial.println("DoubleClicked");
+    //Serial.println("Relais " + String(time_ds, DEC) + " ms ON");
     digitalWrite(ledPin, ON);
   } else {
-    tickOccured = false;
     digitalWrite(grinderPin, LOW);
     os_timer_disarm(&myTimer);
-    Serial.println("Abort!");
-    Serial.println("Relais OFF");
+    clickOccured = false;
+    //Serial.println("Abort!");
+    //Serial.println("Relais OFF");
     digitalWrite(ledPin, OFF);
   }
 }
@@ -73,16 +90,17 @@ void press() {
     digitalWrite(grinderPin, HIGH);  // turn Relais ON
     os_timer_arm(&myTimer, time_max, false);
     pressOccured = true;
-    Serial.println("Pressed");
-    Serial.println("Relais ON");
+    relaisTime = time_max;
+    relaisStart = millis();
+    //Serial.println("Pressed");
+    //Serial.println("Relais ON");
     digitalWrite(ledPin, ON);
   } else {
-    tickOccured = false;
     digitalWrite(grinderPin, LOW);
     os_timer_disarm(&myTimer);
     pressOccured = false;
-    Serial.println("Abort!");
-    Serial.println("Relais OFF");
+    //Serial.println("Abort!");
+    //Serial.println("Relais OFF");
     digitalWrite(ledPin, OFF);
   }
 }
@@ -145,58 +163,114 @@ void handleRoot() {
 
 void handleSave() {
   if (server.arg("ss")!= "") {
-    Serial.println("Singleshot: " + server.arg("ss"));
+    //Serial.println("Singleshot: " + server.arg("ss"));
     time_ss = server.arg("ss").toInt();
     eeWriteInt(0, server.arg("ss").toInt());
   }
   if (server.arg("ds")!= "") {
-    Serial.println("Doubleshot: " + server.arg("ds"));
+    //Serial.println("Doubleshot: " + server.arg("ds"));
     time_ds = server.arg("ds").toInt();
     eeWriteInt(4, server.arg("ds").toInt());
   }
 
 }
 
-// handle WiFi and connect if not connected
 void handleWifi() {
-  static bool oldWifiStaus = false;
-  static unsigned long wifi_start = millis();
-  unsigned long wifi_int = 30000;
+  // handle WiFi and connect if not connected
+  static bool lastWifiStaus = false;
+  static unsigned long wifi_restart = millis();
+  unsigned long wifi_int = 500;
 
-  if (WiFi.status() != WL_CONNECTED && millis() > wifi_start + wifi_int) {
-    // Connecting to a WiFi network
-    Serial.println("WiFi not connected");
-    Serial.print("Trying to connect to: ");
-    Serial.println(ssid);
-    WiFi.begin(ssid, password);
-    oldWifiStaus = false;
-    wifi_start = millis();
-  }
-
-  if (oldWifiStaus == false && WiFi.status() == WL_CONNECTED) {
+  if (WiFi.status() == WL_CONNECTED) {
     // Connected to WiFi
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-    oldWifiStaus = true;
-
-    if (!MDNS.begin("sonoffgrinder")) {             // Start the mDNS responder for esp8266.local
-      Serial.println("Error setting up MDNS responder!");
+    if (lastWifiStaus == false) {
+      // Newly connected to WiFi
+      //Serial.println("WiFi connected");
+      //Serial.println("IP address: ");
+      //Serial.println(WiFi.localIP());
+      lastWifiStaus = true;
+      if (!MDNS.begin("sonoffgrinder")) {             // Start the mDNS responder for esp8266.local
+        //Serial.println("Error setting up MDNS responder!");
+      }
+      //Serial.println("mDNS responder started");
     }
-    Serial.println("mDNS responder started");
+    display.setTextAlignment(TEXT_ALIGN_RIGHT);
+    display.drawString(128, 54, WiFi.localIP().toString());
   }
 
+  if (WiFi.status() != WL_CONNECTED) {
+    // Waiting to connect to a WiFi network
+    //Serial.println("WiFi not connected");
+    //Serial.print("Trying to connect to: ");
+    //Serial.println(ssid);
+    if (lastWifiStaus == true) {
+      // (Re)start WiFi 
+      WiFi.begin(ssid, password);
+      lastWifiStaus = false;
+      wifi_restart = millis();
+    }
+    if (millis() > wifi_restart) {
+      wifi_restart = millis() + wifi_int;
+      delay(50); // pauses the sketch and allows WiFi and TCP/IP tasks to run
+    }
+    display.setTextAlignment(TEXT_ALIGN_RIGHT);
+    display.drawString(128, 54, ssid);
+  }
+
+  yield();
 }
 
-// start of timerCallback
+void handleDisplay() {
+  // Handle Display
+  
+  if(clickOccured == true){
+    // display grinding progress
+    display.setTextAlignment(TEXT_ALIGN_CENTER);
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(64, 16, "grinding " + String(relaisTime) + " ms");
+    display.drawProgressBar(0, 38, 120, 10, (millis() - relaisStart) / (relaisTime / 100));
+    //display.drawString(64, 45, String((millis() - relaisStart) / (relaisTime / 100)) + " %");
+  }
+
+  if(pressOccured == true){
+    // display grinding setup progress
+    display.setTextAlignment(TEXT_ALIGN_RIGHT);
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(128, 16, "grinding " + String(millis() - relaisStart) + " ms");
+    display.setTextAlignment(TEXT_ALIGN_CENTER);
+    display.drawProgressBar(0, 38, 120, 10, (millis() - relaisStart) / (relaisTime / 100));
+  }
+
+  if(clickOccured == false && pressOccured == false){
+    display.setTextAlignment(TEXT_ALIGN_RIGHT);
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(128, 16, "Click " + String(time_ss) + " ms");
+    display.drawString(128, 34, "Double " + String(time_ds) + " ms");
+  }
+  
+  // Overlay
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0, 0, "SonoffGrinder");
+  display.setTextAlignment(TEXT_ALIGN_RIGHT);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(128, 0, VERSION);
+  display.display();
+}
+
 void timerCallback(void *pArg) {
-  tickOccured = true;
+  // start of timerCallback
+  digitalWrite(grinderPin, LOW);
+  //Serial.println("Timer ausgelaufen");
+  //Serial.println("Relais OFF");
+  digitalWrite(ledPin, OFF);
+  os_timer_disarm(&myTimer);
+  clickOccured = false;
 }
-
 
 // *******SETUP*******
 void setup() {
-  Serial.begin(115200); // Start serial
+  //Serial.begin(115200); // Start serial
 
 //======HARDWARE PART======================================================
   pinMode(grinderPin, OUTPUT);      // define grinder output pin
@@ -206,9 +280,15 @@ void setup() {
 
   pinMode(14, INPUT_PULLUP);        // Bugfix ttbounce button.enablePullup(); not working
 
+  display.init();
+  display.flipScreenVertically();
+  handleDisplay();
+
 //======Wifi PART==========================================================
-  Serial.print("Connecting to: ");
-  Serial.println(ssid);
+  //Serial.print("Connecting to: ");
+  //Serial.println(ssid);
+  WiFi.mode(WIFI_STA); // explicitly set the ESP8266 to be a WiFi-client
+  WiFi.persistent(false); // do not store settings in EEPROM
   WiFi.begin(ssid, password);
   yield();
 
@@ -217,7 +297,7 @@ void setup() {
   server.on ("/save", handleSave);
 
   server.begin();
-  Serial.println ( "HTTP server started" );
+  //Serial.println ( "HTTP server started" );
 
 //======LOKALER PART======================================================
   os_timer_setfn(&myTimer, timerCallback, NULL);
@@ -255,27 +335,54 @@ void setup() {
     }
 
     // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-    Serial.println("Start updating " + type);
+    //Serial.println("Start updating " + type);
   });
   ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
+    //Serial.println("\nEnd");
+    display.clear();
+    display.setTextAlignment(TEXT_ALIGN_CENTER);
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(64, 10, "OTA Finsihed");
+    display.drawProgressBar(0, 32, 120, 10, 100);
+    display.drawString(64, 45, "rebooting..");
+    display.display();
+    // eeWriteInt(0, 1000); // for testing
+    // eeWriteInt(4, 2000); // for testing
   });
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    //Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    display.clear();
+    display.setTextAlignment(TEXT_ALIGN_CENTER);
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(64, 10, "OTA Update");
+    display.drawProgressBar(0, 32, 120, 10, progress / (total / 100));
+    display.drawString(64, 45, String(progress / (total / 100)) + " %");
+    display.display();
   });
   ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
+    //Serial.printf("Error[%u]: ", error);
+    display.clear();
+    display.setTextAlignment(TEXT_ALIGN_CENTER);
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(64, 10, "OTA Error");
+    display.drawProgressBar(0, 32, 120, 10, 100);
     if (error == OTA_AUTH_ERROR) {
-      Serial.println("Auth Failed");
+      //Serial.println("Auth Failed");
+      display.drawString(64, 50, "Auth Failed");
     } else if (error == OTA_BEGIN_ERROR) {
-      Serial.println("Begin Failed");
+      //Serial.println("Begin Failed");
+      display.drawString(64, 50, "Begin Failed");
     } else if (error == OTA_CONNECT_ERROR) {
-      Serial.println("Connect Failed");
+      //Serial.println("Connect Failed");
+      display.drawString(64, 50, "Connect Failed");
     } else if (error == OTA_RECEIVE_ERROR) {
-      Serial.println("Receive Failed");
+      //Serial.println("Receive Failed");
+      display.drawString(64, 50, "Receive Failed");
     } else if (error == OTA_END_ERROR) {
-      Serial.println("End Failed");
+      //Serial.println("End Failed");
+      display.drawString(64, 50, "End Failed");
     }
+    display.display();
   });
   ArduinoOTA.begin();
 
@@ -284,36 +391,30 @@ void setup() {
 
 // *******LOOP*******
 void loop() {
+  display.clear();
 //======SERVER PART======================================================
   handleWifi(); // check Wifi
   ArduinoOTA.handle();
   server.handleClient();
-  yield();
 //======LOKALER PART======================================================
   button.update();
 
-  if (pressOccured == true) {
+  if(pressOccured == true) {
     // press was detected
     if (button.getHoldTime() > 0) {
+      // while button is pressed
       holdTime = (int)button.getHoldTime(); // hold button time
     }
     if (button.read() == LOW) { // wait for button release
       holdTime = holdTime - pressInterval; // substract press detection time from hold time
       eeWriteInt(4, holdTime); // safe doublshot time
-      tickOccured = true;
       pressOccured = false;
       time_ds = holdTime;
-      Serial.println("Doubleshot set to " + String(holdTime, DEC) + " ms");
+      os_timer_arm(&myTimer, 0, false); // instantly fire timer
+      //Serial.println("Doubleshot set to " + String(holdTime, DEC) + " ms");
     }
   }
 
-  if (tickOccured == true) {
-     tickOccured = false;
-     digitalWrite(grinderPin, LOW);
-     Serial.println("Timer ausgelaufen");
-     Serial.println("Relais OFF");
-     digitalWrite(ledPin, OFF);
-     os_timer_disarm(&myTimer);
-  }
-
+  handleDisplay();
+  delay(10);
 }
